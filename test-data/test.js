e92ea38f87811f9ffa94e6997ba5d4f725d393d5
arhandev <farhan.abdulhamid11@gmail.com>
2025-08-12 10:23:17 +0700

    last update, add testing crawling data

---
+++ test-data/test.js
@@ -0,0 +1,14 @@
+[
+  {
+    id: Integer(0),
+    first_name: String("Farhan"),
+    last_name: String("Abdul Hamid"),
+  },
+  { id: Integer(1), first_name: String("Akbar"), last_name: String("Maulana") },
+  { id: Integer(2), last_name: String("Haryadi"), first_name: String("Daffa") },
+  {
+    last_name: String("Ramadhan"),
+    id: Integer(3),
+    first_name: String("Hanif"),
+  },
+];
